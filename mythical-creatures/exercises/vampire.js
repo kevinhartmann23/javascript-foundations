f25b6d1034a16c339cc31fbdfe4a16b595e16589
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-10-15 08:51:50 -0600

    Add Vampire.js - All tests passed

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,17 @@
+class Vampire {
+  constructor(name, pet) {
+    this.name = name;
+    if (pet === undefined){
+      this.pet = "bat";
+    } else {
+      this.pet = pet;
+    }
+    this.thirsty = true;
+  };
+drink() {
+  this.thirsty = false;
+};
+};
+
+
+module.exports = Vampire;
